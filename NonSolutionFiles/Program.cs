c44da7197155c0b7d1df314edaccb3442d2d02a6
Roger Kratz <roger.kratz@gmail.com>
2015-09-30 13:34:52 +0200

    Adding some comments on how to use the console application.

--- NonSolutionFiles/Program.cs
+++ NonSolutionFiles/Program.cs
@@ -6,14 +6,23 @@ namespace NonSolutionFiles
 	{
 		static void Main(string[] args)
 		{
-			var solutionPath = args[0];
-			var fileReader = new FileReader();
-			var findNonSolutionFiles = new FindNonSolutionFiles(new FilesOnDisk(), new FilesInProject(fileReader), new ProjectsInSolution(fileReader));
-			foreach (var deadFile in findNonSolutionFiles.Find(solutionPath))
+			if (args.Length != 1)
 			{
-				Console.WriteLine(deadFile);
+				Console.WriteLine("Usage:");
+				Console.WriteLine("NonSolutionFiles.exe [absolute path to your c# solution file]");
+				Console.ReadKey();
 			}
-			Console.ReadKey();
+			else
+			{
+				var solutionPath = args[0];
+				var fileReader = new FileReader();
+				var findNonSolutionFiles = new FindNonSolutionFiles(new FilesOnDisk(), new FilesInProject(fileReader), new ProjectsInSolution(fileReader));
+				foreach (var deadFile in findNonSolutionFiles.Find(solutionPath))
+				{
+					Console.WriteLine(deadFile);
+				}
+				Console.ReadKey();
+			}
 		}
 	}
 }
